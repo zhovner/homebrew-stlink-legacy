261b00271ca4ce052ab16cd7e3ac064258bd71ff
Pavel Zhovner <pavel@zhovner.com>
2020-09-29 17:36:30 +0300

    initial commit

---
+++ stlink-legacy.rb
@@ -0,0 +1,20 @@
+class StlinkCustom < Formula
+  desc "Legacy STM32 discovery line Linux programmer"
+  homepage "https://github.com/texane/stlink"
+  url "https://github.com/texane/stlink/archive/v1.5.1.tar.gz"
+  sha256 "e0145fbfd3e781f21baf12a0750b0933c445ee6338e36142836bf5a2c267e107"
+  license "BSD-3-Clause"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libusb"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    assert_match "st-flash #{version}", shell_output("#{bin}/st-flash --debug reset 2>&1", 255)
+  end
+end
